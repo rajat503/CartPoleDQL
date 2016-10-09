37dfb5cef1bb669805b9f2dfb676f53afa3d27ff
rajat503 <rajat503@gmail.com>
2016-10-09 18:55:41 +0530

    added experience replay

---
+++ exp_replay.py
@@ -0,0 +1,31 @@
+from collections import deque
+import dql_sample
+import gym
+import random
+
+env = gym.make('CartPole-v0')
+
+replay_memory = deque()
+
+
+for i_episode in range(5000):
+    observation = env.reset()
+    for t in range(1000):
+        env.render()
+        s=observation
+        action = dql_sample.getAction(observation)
+        observation, reward, done, info = env.step(action)
+
+        if len(replay_memory) == 2000 :
+            replay_memory.popleft()
+        replay_memory.append([s, action, reward, observation])
+
+        if len(replay_memory) < 100:
+            dql_sample.learn([[s, action, reward, observation]])
+        else:
+            sample = [ replay_memory[i] for i in random.sample(range(len(replay_memory)), 100) ]
+            dql_sample.learn(sample)
+
+        if done:
+            print("Episode {} finished after {} timesteps".format(i_episode, t+1))
+            break
